2f1fab7931d0b32b6dd99632f3c10de9ad1e6a19
ocaocuoci <hernandar0606@gmail.com>
2024-11-06 21:34:32 +0700

    percobaan 3 nomer 1

--- Numbers12.java
+++ Numbers12.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Numbers12{
     public static void main(String[] args){
 
@@ -5,5 +6,9 @@ public class Numbers12{
            myNumbers[0] = new int[5];  
            myNumbers[1] = new int[3];  
            myNumbers[2] = new int[1];  
+
+           for(int i=0; i < myNumbers.length; i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+           }
     }
 }
