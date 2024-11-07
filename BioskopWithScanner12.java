189a1a3e3f808de7c8f8436ebc6b753452e2dbdc
ocaocuoci <hernandar0606@gmail.com>
2024-11-07 07:09:40 +0700

    belum selesai

--- BioskopWithScanner12.java
+++ BioskopWithScanner12.java
@@ -8,11 +8,22 @@ public class BioskopWithScanner12{
         String nama, next;
         String[][] penonton =new String[4][2];
 
-        while (true){
+        System.out.println("PILIHAN MENU");
+        System.out.println("1. Input data penonton");
+        System.out.println("2. Tampilkan daftar penonton");
+        System.out.println("3. Exit");
+        System.out.print("Masukkan pilihan menu (1-3): ");
+        int pilihan = input.nextInt();
+
+        switch(pilihan){
+            case 1:
             System.out.print("Masukkan nama: ");
             nama = input.nextLine();
+            input.nextLine();
+
             System.out.print("Masukkan baris: ");
             baris = input.nextInt();
+
             System.out.print("Masukkan kolom: ");
             kolom = input.nextInt();
             input.nextLine();
@@ -26,9 +37,14 @@ public class BioskopWithScanner12{
                 break;
             }
 
-    
+            case 2:
+            for (int i = 0; i < penonton.length; i++){
+                System.out.println("Penonton pada baris ke-" + (i +1) + ": " String.join(", ", penonton[i]));
+            }
+
 
         }
+
     
     }
 }
