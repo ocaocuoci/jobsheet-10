39353ef60bc08083c65b3ca9ed367d0155c47615
ocaocuoci <hernandar0606@gmail.com>
2024-11-07 08:30:58 +0700

    tugas 1 membuat rata responden, pertanyaan, dan keseluruhan

--- Tugas1.java
+++ Tugas1.java
@@ -3,9 +3,8 @@ public class Tugas1{
     public static void main (String [] args){
         Scanner input =new Scanner(System.in);
 
-        double rataResponden = 0;
-        double rataPertanyaan = 0;
-        double rataKeseluruhan = 0;
+        double rataResponden =0, rataKeseluruhan = 0, rataPertanyaan;
+        double totalNilai=0;
         int nilai;
 
         int [][] survey = new int[10][6];
@@ -16,12 +15,27 @@ public class Tugas1{
             for (int j = 0; j < survey[i].length; j++){
                 System.out.print("Input nilai dari 1-5 untuk pertanyaan ke-" + (j + 1) + ": ");
                     nilai = input.nextInt();
-
+                    survey[i][j] = nilai;
+                    rataResponden += nilai;
+                    totalNilai += nilai;
            
             }
-            rataResponden += survey.length;
-            double rataResponden /= survey[i].length;       
+            rataResponden /= survey[i].length;       
             System.out.println("Rata rata Responden: " + rataResponden);
+            System.out.println();
         }
+        for(int j = 0; j < survey[0].length; j++ ){
+            rataPertanyaan =0;
+
+            for(int i =0; i < survey.length; i++){
+                rataPertanyaan += survey[i][j];
+            }
+            rataPertanyaan /= survey.length;
+            System.out.println("Rata rata pertanyaan ke-" + (j + 1) + ": " + rataPertanyaan);
+        }
+        System.out.println();
+        rataKeseluruhan = totalNilai / (survey.length * survey[0].length);
+        System.out.println("Rata rata keselurusan " + rataKeseluruhan);
+
     }
 }
